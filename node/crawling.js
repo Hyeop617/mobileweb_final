f75849ec7179e582f3e8207fa45be79d695a0a3b
hyeop <totoll13mail@gmail.com>
2019-11-06 02:42:02 +0900

    skysport crawling

--- node/crawling.js
+++ node/crawling.js
@@ -1,51 +1,51 @@
 const request = require('request');
-//const axios = require('axios');
 const cheerio = require('cheerio');
-    
 
-//const url = "https://www.premierleague.com/fixtures";
-const url = "https://www.flashscore.co.uk/football/england/premier-league/fixtures/";
-//const url = "http://www.naver.com";
 
-//const getHtml = async () =>{
-//    try{
-//        return await axios.get("https://www.skysports.com/premier-league-fixtures");
-//    }catch(error){
-//        console.error(error);
-//    }
-//}
-//getHtml().then(html => {
-//    var $ = cheerio.load(html.data);
-//    console.log($(this).find('#widgetLite-6 > div:nth-child(3) > a > span.matches__item-col.matches__participant.matches__participant--side1 > span > span').text());
-//    console.log($(this).find('#widgetLite-0 > div.grid__col.site-layout-primary__col1 > div.page-header > h1 > a > abbr > span').text());
-//});
+var options = {
+    url: 'https://www.foxsports.com/soccer/schedule?competition=1',
+    method: 'POST',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.146 Whale/2.6.88.11 Safari/537.36',
+    }
+};
 
-request(url, (error, response, body) =>{
-    if (error) throw error;
-//    console.log(body);
-    
-    var $ = cheerio.load(body);
-    
-    
-    try{
-        let timearr = [];
-        let matcharr = [];
-//        const time = $("#mainContent > div.tabbedContent > div.wrapper.col-12.active > div.col-12 > section").children("time.long");
-        const match = $("#live-table > div > div > div").children("div.event__match event__match--static event__match--scheduled event__match--oneLine");
-//        
-        match.each(function(i, elem){
-            var home = $(this).find('div.event__participant event__participant--home').text();
-            var away = $(this).find('div.event__participant event__participant--away').text();
-            console.log(home, away);
+request(options, (error, response, body) => {
+    //    console.log(body);
+
+    try {
+        var match = [];
+        var home = [];
+        var away = [];
+        var date = [];
+
+        var $ = cheerio.load(body);
+        var table = $('#wisfoxbox > section.wisbb_body > table');
+
+
+        table.find('thead').each(function (index, ele) {
+            date.push($(this).find('tr > th').text());
         });
-//        console.log(matcharr);
-        
-//        var match = "";
-//        match = $(this).find('#widgetLite-6').html();
-//        console.log(match);
-//        console.log($(this).find('#widgetLite-6 > div:nth-child(3) > a > span.matches__item-col.matches__participant.matches__participant--side1 > span > span').text())
-//        console.log($(this).find('#widgetLite-0 > div.grid__col.site-layout-primary__col1 > div.page-header > h1 > a > abbr > span').text())
-    } catch(error){
+
+
+        var bodyList = table.find('tbody');
+        for (var i = 0; i < bodyList.length; i++) {
+            var index = bodyList.eq(i).children().length;
+            var tmpHome = [];
+            var tmpAway = [];
+            for (var j = 0; j < index; j++) {
+                tmpHome[j] = bodyList.eq(i).children().eq(j).find('td.wisbb_team.wisbb_reversed.wisbb_firstTeam > div.wisbb_fullTeamStacked > a > span:nth-child(3)').text();
+                tmpAway[j] = bodyList.eq(i).children().eq(j).find('td.wisbb_team.wisbb_secondTeam > div.wisbb_fullTeamStacked > a > span:nth-child(3)').text();
+                match.push({
+                    date: date[i],
+                    home: tmpHome[j],
+                    away: tmpAway[j]
+                });
+            }
+        }
+        console.log(match);
+
+    } catch (error) {
         console.log(error);
     }
 });
